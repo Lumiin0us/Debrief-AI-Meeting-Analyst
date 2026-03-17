6fa00d3908e2e5fd4a5d5eeee5d40fde3dc98263
Lumiin0us <73905501+Lumiin0us@users.noreply.github.com>
2026-03-17 08:30:44 +0200

    fix case sensitivity on UploadPage component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import UploadPage from "./components/uploadPage";
+import UploadPage from "./components/UploadPage";
 import HistoryPage from "./components/HistoryPage";
 import MeetingPage from "./components/MeetingPage";
 
